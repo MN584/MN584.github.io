f6ad1075c02036bc2d2fb6bced0704d08dd306a5
MN584 <maximilian.neumeie28@student.tamdistrict.org>
2024-10-22 15:12:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let valueOne = 5
 let valueTwo = 7
-slotValueTwo.innerText
+let valueThree = 8
+
+slotValueTwo.innerText = valueTwo
 slotValueOne.innerText = valueOne
+slotValueThree.innerText = valueThree
 
 function spin() {
 let valueOne = Math.floor(Math.random()*10))
-  let valueTwo = Math.floor(Math.random()*10))
-  slotValueOne.innerText = valueOne
+  let valueTwo = Math.floor(Math.random()*10)) 
+    let valueThree = Math.floor(Math.random()*10))
+slotValueTwo.innerText = valueTwo
+slotValueOne.innerText = valueOne
+slotValueThree.innerText = valueThree
 }
+
 spin()
